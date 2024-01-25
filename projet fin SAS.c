7ea0e5941511e6ce0a655309a808e72086d3e2d4
zakaria <banidzakaria@gmail.com>
2024-01-25 11:15:10 +0100

    sas project

---
+++ projet fin SAS.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+void menu(){
+	int x;
+	printf("MENU : \n");
+	printf("1-Ajouter une Tache.\n");
+	printf("2-Afficher la liste des Taches.\n");
+	printf("3-Modifier une Tache.\n");
+	printf("4-Supprimer une Tache.\n");
+	printf("5-Ordonner les Taches.\n");
+	printf("6-Flitrer les Taches.\n");
+	printf("0-QUITTER.\n");
+	printf("Entrer votre choix :\n");
+	scanf("%d", x);
+	switch(x){
+		case 1:
+			
+	}
+	
+}
+int main() {
+	
+	menu();
+	
+	return 0;
+}
