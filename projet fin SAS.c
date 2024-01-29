f8266acf6761994fccb83eaa72b0be212e2962e7
zakaria <banidzakaria@gmail.com>
2024-01-29 15:53:59 +0100

    l'ajoute de la fonction modifier

--- projet fin SAS.c
+++ projet fin SAS.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>	
+#include <string.h>
 //les fonctions.
 void menu();
 
@@ -19,6 +20,7 @@ typedef struct{
 }Taches;
 Taches t[100];
 int taille = 0;
+int i;
 // déf de fonction d'ajouter une tache.
 
 void Addtache(){
@@ -26,29 +28,32 @@ void Addtache(){
 	int i;
 	for(i=taille ; i < taille+1 ; i++){
 
-	printf("veuillez entrer le nom du Tache numero %d: \n", i+1);
+	printf("veuillez entrer le nom du Tache numero %d: ", i+1);
 	scanf(" %[^\n]s", &t[i].name);
-	printf("veuillez ajouter une dsicription : \n");
+	printf("veuillez ajouter une dsicription : ");
 	scanf(" %[^\n]s", &t[i].details);
 	printf("veuilez entrer la date d'echeance : \n");
-	printf("entrer le jour : \n");
+	printf("entrer le jour : ");
 	scanf("%d", &t[i].d.jour);
-	printf("entrer le mois : \n");
+	printf("entrer le mois : ");
 	scanf("%d", &t[i].d.mois);
-	printf("entrer l'annee : \n");
+	printf("entrer l'annee : ");
 	scanf("%d", &t[i].d.annee);
-	printf("ajouter la priorite : \n");
+	printf("ajouter la priorite : ");
 	scanf("%d", &t[i].priorite);
 	printf("tache ajouter. \n");
 	
 	}
 	taille++;
+	
+	printf("______________________________________________________ \n\n");
 	menu();
 }
 
 //fonction d'afficher.
 void Afficher(){
 	for(int i=0 ; i<taille ; i++){
+		printf("->Les taches enregistrer sont : \n\n");
 		printf("\t -nom de la tache : %s \n", t[i].name);
 		printf("\t -discription : %s \n", t[i].details);
 		printf("\t -date d'echeance : %d %d %d \n", t[i].d.jour, t[i].d.mois, t[i].d.annee);
@@ -56,12 +61,72 @@ void Afficher(){
 	}
 	menu();
 }
+//Fonction de la recherche.
+int recherche(char mod[20]){
+	for(i=0 ; i<taille ; i++){
+		int res = strcmp (t[i].name, mod);
+		if(res == 0){
+			return i;
+		}
 	
+	}
+	return -1;
+}
+
+//Déf de la Fonction Modifier.
+void Modifier(){
+	char mod[20];
+	printf("entrer le nom du tache que vous voullez modifier : ");
+	scanf("%s", mod);
+	int res = recherche(mod);
+		if(res != -1 ){
+			printf("donner le nouveau nom du tache : ");
+			scanf(" %[^\n]s", &t[res].name);
+			printf("donner la nouvelle description : ");
+			scanf(" %[^\n]s", &t[res].details);
+			printf("veuilez entrer la nouvelle date d'echeance : \n");
+			printf("entrer le jour : ");
+			scanf("%d", &t[res].d.jour);
+			printf("entrer le mois : ");
+			scanf("%d", &t[res].d.mois);
+			printf("entrer l'annee : ");
+			scanf("%d", &t[res].d.annee);
+			printf("ajouter la priorite : ");
+			scanf("%d", &t[res].priorite);
+			printf("tache a ete modifier. ");
+		}
+		else
+		printf("Erreur, cette tache n'existe pas \n");
+		
+	menu();
+	}
+	
+//Fonction supprimer.
+void supprimer(){
+	char sup[20];
+		printf("Entrer le nom du tache que vous voullez suprimer : ");
+		scanf("%s", sup);
+		int res = recherche(sup);
+		if(res != -1 ){
+		for(i=res ; i<taille ; i++){
+			t[res] = t[res+1];
+		}
+		printf("la tache %s a ete suprimmer. \n", sup);
+		
+		taille--;
+			
+		}
+		else{
+		printf("Erreur, cette tache n'existe pas");
+		}
+		menu();
+}
+
 
 // fonction "MENU".
 void menu(){
 	int x;
-	printf("\n\n\t\t*****************MENU*******************\n\n");
+	printf("\n\n*****************MENU PRINCIPAL : *******************\n\n");
 	printf("1-Ajouter une Tache.\n");
 	printf("2-Afficher la liste des Taches.\n");
 	printf("3-Modifier une Tache.\n");
@@ -85,6 +150,12 @@ void menu(){
 		break;
 		case 2:
 			Afficher();
+		break;
+		case 3:
+			Modifier();
+		break;
+		case 4:
+			supprimer();
 		break;
 	}
 }
