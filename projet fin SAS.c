394a192868ea2b0bca03aea9785bd841c56e9907
zakaria <banidzakaria@gmail.com>
2024-01-29 11:26:51 +0100

    l'ajoute de l'affichage

--- projet fin SAS.c
+++ projet fin SAS.c
@@ -1,6 +1,64 @@
 #include <stdio.h>
-#include <stdlib.h>
-// fonction d'affichage "menu"
+#include <stdlib.h>	
+//les fonctions.
+void menu();
+
+// structure des taches.
+typedef struct{
+	int jour;
+	int mois;
+	int annee;
+}date;
+
+typedef struct{
+	char name[20];
+	char details[50];
+	date d;
+	int priorite;
+	
+}Taches;
+Taches t[100];
+int taille = 0;
+// déf de fonction d'ajouter une tache.
+
+void Addtache(){
+	
+	int i;
+	for(i=taille ; i < taille+1 ; i++){
+
+	printf("veuillez entrer le nom du Tache numero %d: \n", i+1);
+	scanf(" %[^\n]s", &t[i].name);
+	printf("veuillez ajouter une dsicription : \n");
+	scanf(" %[^\n]s", &t[i].details);
+	printf("veuilez entrer la date d'echeance : \n");
+	printf("entrer le jour : \n");
+	scanf("%d", &t[i].d.jour);
+	printf("entrer le mois : \n");
+	scanf("%d", &t[i].d.mois);
+	printf("entrer l'annee : \n");
+	scanf("%d", &t[i].d.annee);
+	printf("ajouter la priorite : \n");
+	scanf("%d", &t[i].priorite);
+	printf("tache ajouter. \n");
+	
+	}
+	taille++;
+	menu();
+}
+
+//fonction d'afficher.
+void Afficher(){
+	for(int i=0 ; i<taille ; i++){
+		printf("\t -nom de la tache : %s \n", t[i].name);
+		printf("\t -discription : %s \n", t[i].details);
+		printf("\t -date d'echeance : %d %d %d \n", t[i].d.jour, t[i].d.mois, t[i].d.annee);
+		printf("\t -pritorite : %d \n", t[i].priorite);
+	}
+	menu();
+}
+	
+
+// fonction "MENU".
 void menu(){
 	int x;
 	printf("\n\n\t\t*****************MENU*******************\n\n");
@@ -12,41 +70,12 @@ void menu(){
 	printf("6-Flitrer les Taches.\n");
 	printf("0-QUITTER.\n");
 	
-// structure des taches.
-typedef struct{
-	char name[20];
-	char details[50];
-	int date;
-	int priorite;
-	
-}Taches;
-Taches t[100];
-// déf de fonction d'ajouter une tache.
-
-void Addtache(){
-	
-	int i;
-	for(i=0 ; i < 100 ; i++);
-	printf("veuillez entrer le nom du Tache : \n");
-	scanf(" %[^\n]s", &t[i].name);
-	printf("veuillez ajouter une dsicription : \n");
-	scanf(" %[^\n]s", &t[i].details);
-	printf("veuilez entrer la date d'echeance : \n");
-	scanf("%d", &t[i].date);
-	printf("ajouter la priorite : \n");
-	scanf("%d", &t[i].priorite);
-	
-	menu();
-	
-}
-
-
 	do{
 		printf("Entrer votre choix :\n");
 		scanf("%d", &x);
 		if(x<0 || x>6)
 		printf("votre choix doit etre compris entre 0 et 6. \n");
-		
+	
 	}while(x<0 || x>6);
 	
 	
@@ -54,17 +83,17 @@ void Addtache(){
 		case 1:
 			Addtache();
 		break;
+		case 2:
+			Afficher();
+		break;
 	}
-
 }
 
-
-
 // debut du main
-int main() { 
+int main() {
 	
 	menu();
 	
-	return 0;
+return 0;
 }
 // fin du main
